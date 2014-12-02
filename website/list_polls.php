bd77df9c8ea6f2408e518d0523ffad5dc4768b98
Joao <netforum@gmail.com>
2014-12-02 19:15:59 +0000

    last try

--- website/list_polls.php
+++ website/list_polls.php
@@ -4,7 +4,7 @@
   include_once("database/connection.php");
   include_once("database/polls.php");
 
-  $result = getAllPools();
+  $result = getAllPolls();
   
   include_once("templates/header.php");  
   include_once("templates/featurettes.php");  
