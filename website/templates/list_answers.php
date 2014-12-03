0d22a832c4ff6dcfea2cf4c1d757db891314eed4
CatarinaTerra <catarina.cct@gmail.com>
2014-12-03 19:45:59 +0000

    v1.2

--- website/templates/list_answers.php
+++ website/templates/list_answers.php
@@ -2,8 +2,7 @@
   <h3>Answers</h3>
   <? foreach ($answers as $answer) { ?>
   <div class="answer">
-    <p class="text"><?=$comment['text']?></p>
-    <p class="author">Answered by: <?=$answer['author']?></p>
+    <p class="text"><?=$answer['text']?></p>
   </div>
   <? } ?>
 </div>
---
+++ website/templates/view_poll.php
@@ -0,0 +1,21 @@
+<div class="col-md-12 column">
+	<img alt="140x140" src="http://lorempixel.com/140/140/" class="img-thumbnail" />
+		<h3>
+			Question
+		</h3>
+<form role="form">
+		<div class="radio">
+          <label><input type="radio" name="optradio">Option 1</label>
+        </div>
+        <div class="radio">
+          <label><input type="radio" name="optradio">Option 2</label>
+        </div>
+        <div class="radio">
+          <label><input type="radio" name="optradio">Option 3</label>
+        </div>
+      </form>
+   <button type="button" class="btn btn-info">Vote</button>
+	</div>	 
+</div>
+
+   
