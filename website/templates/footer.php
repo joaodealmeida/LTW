7922442ada7c461e0600c83d0b5863937cecc19f
feup09 <brunocoelhodasilva1991@gmail.com>
2014-12-04 17:25:52 +0000

    footer

--- website/templates/footer.php
+++ website/templates/footer.php
@@ -11,6 +11,8 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/docs.min.js"></script>
+	<script src="js/jscript.js"></script>
+	<script src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
     <script src="js/ie10-viewport-bug-workaround.js"></script>
   </body>
