f13413b67388bcb634e110533779cd9e57fe89f5
CatarinaTerra <catarina.cct@gmail.com>
2014-12-05 15:55:36 +0000

    failed centrados

--- website/templates/register_failed.php
+++ website/templates/register_failed.php
@@ -1,8 +1,10 @@
 	<div class="container pollview">
 	<div class="col-md-12 column">
-			<h3>
+			<h3 class="text-center">
 				Register failed. Unavailable username/password.
 			</h3>
+			<div style="text-align:center"> 
 			<a href="register.php" class="btn btn-info">Try Again</a>
+			</div>
 		</div>
 </div>
